829cbda4717415cb47ce2de1b84c43184a4074a3
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-07-25 22:53:43 +0300

    add styled

--- src/components/feedbackOptions/FeedbackOptions.js
+++ src/components/feedbackOptions/FeedbackOptions.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ProtoType from "prop-types";
 import { v4 as uuidv4 } from "uuid";
+import css from "./FeadbackOptions.module.css";
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
       {options.map((option) => (
         <button
+          className={css.button}
           key={uuidv4()}
           type="button"
           name={option}
